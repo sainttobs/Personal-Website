33bcc56f0c4ce4d31d5fc258a07f5314c5ceb9c3
sainttobs <sainttobs@gmail.com>
2019-05-09 02:58:42 +0100

    ashshhs

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,5 @@
-var dataCacheName = 'adeyefa-oluwatoba-site';
-var cacheName = 'adeyefa-oluwatoba-site'; 
+var dataCacheName = 'hostelier-v1';
+var cacheName = 'hostelier-final-1'; 
 
 var filesToCache = [
   '/',
@@ -59,35 +59,11 @@ self.addEventListener('fetch', function(e) {
       );
     }
 });
-
-
-var deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', function (e) {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-
-  showAddToHomeScreen();
-
-});
-
-
-function showAddToHomeScreen() {
-
-  var a2hsBtn = document.querySelector(".ad2hs-prompt");
-
-  a2hsBtn.style.display = "block";
-
-  a2hsBtn.addEventListener("click", addToHomeScreen);
-
-}
-
+  
 
 
 function addToHomeScreen() {  var a2hsBtn = document.querySelector(".ad2hs-prompt");  // hide our user interface that shows our A2HS button
-  a2hsBtn.style.display = '#FFF';  // Show the prompt
+  a2hsBtn.style.display = 'none';  // Show the prompt
   deferredPrompt.prompt();  // Wait for the user to respond to the prompt
   deferredPrompt.userChoice
     .then(function(choiceResult){
@@ -101,3 +77,5 @@ function addToHomeScreen() {  var a2hsBtn = document.querySelector(".ad2hs-promp
   deferredPrompt = null;
 
 });}
+
+  addToHomeScreen();
